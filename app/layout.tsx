5120ae1fa50c69277eae338e602c7477f46f79ed
odedahay <odedahay@yahoo.com>
2025-05-21 14:55:44 -0600

    Stage: 1 add ShadCN

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 const geistMono = Geist_Mono({
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn("min-h-screen bg-background font-sans antialiased", geistSans.variable )}
       >
         {children}
       </body>
