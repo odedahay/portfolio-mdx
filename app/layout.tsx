4eea00fe06401e50ffbae6687ae936b78157d88f
Rodel Dahay <odedahay@gmail.com>
2026-09-17 02:02:46 -0600

    remove GA4 test

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,19 +44,14 @@ export default function RootLayout({
         )}
         suppressHydrationWarning
       >
-        {/* Start GA4 code here */}
-
+       
         {/* <!-- Google tag (gtag.js) --> */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-W5TKC5V67E"></Script>
+        {/* <Script async src="https://www.googletagmanager.com/gtag/js?id=G-W5TKC5V67E"></Script>
         <Script id="google-analytics" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-W5TKC5V67E');
+      
           `}
-        </Script>
+        </Script> */}
 
         {/* End  */}
         <Providers>
